24299f64ec549f6d2b245e27962d138136cc7f03
fatihalayyub <naufalabrarrabbanii@gmail.com>
2022-05-18 12:25:34 +0700

    update mentor fauzi

--- app/Models/Operational/Appointment.php
+++ app/Models/Operational/Appointment.php
@@ -4,9 +4,11 @@ namespace App\Models\Operational;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
+    // use HasFactory;
     use SoftDeletes;
 
     // declare table
@@ -32,4 +34,30 @@ class Appointment extends Model
         'updated_at',
         'deleted_at',
     ];
+
+    // one to many
+    public function doctor()
+    {
+        // 3 parameter (path model, field foreign key, field primary key from table hasMany/hasOne)
+        return $this->belongsTo('App\Models\Operational\Doctor', 'doctor_id', 'id');
+    }
+
+    public function consultation()
+    {
+        // 3 parameter (path model, field foreign key, field primary key from table hasMany/hasOne)
+        return $this->belongsTo('App\Models\MasterData\Consultation', 'consultation_id', 'id');
+    }
+
+    public function user()
+    {
+        // 3 parameter (path model, field foreign key, field primary key from table hasMany/hasOne)
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
+
+    // one to many
+    public function transaction()
+    {
+        // 2 parameter (path model, field foreign key)
+        return $this->hasOne('App\Models\Operational\Transaction', 'appointment_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,24 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
-    use HasFactory;
+    // use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+    use SoftDeletes;
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+        'email_verified_at',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -58,4 +68,29 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    // many to many --- //
+    public function role()
+    {
+        return $this->belongsToMany('App\Models\ManagementAccess\Role');
+    }
+
+    // one to many
+    public function appointment()
+    {
+        // 2 parameter (path model, field foreign key)
+        return $this->hasMany('App\Models\Operational\Appointment', 'user_id');
+    }
+
+    public function detail_user()
+    {
+        // 2 parameter (path model, field foreign key)
+        return $this->hasOne('App\Models\ManagementAccess\DetailUser', 'user_id');
+    }
+
+    public function role_user()
+    {
+        // 2 parameter (path model, field foreign key)
+        return $this->hasMany('App\Models\ManagementAccess\RoleUser', 'user_id');
+    }
 }
